da3edee24c93fef2977bff855d6e849d1ca2bc49
juanquinnterodev <896@holbertonschool.com>
2019-12-10 02:32:22 +0000

    create and readme

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create file in /tmp
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
